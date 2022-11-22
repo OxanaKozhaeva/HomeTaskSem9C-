eb64f97933d48368ca16350575320d8a81968257
Oxana Kozhaeva <kozhaeva_1@mail.ru>
2022-11-22 17:07:39 +0300

    Add SumPositiveIntFromMtoN

---
+++ SumPositiveIntFromMtoN/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+for (int i = m; i <=n; i++)
+{
+    sum +=i;   
+}
+Console.WriteLine("Сумма натуральных чисел в промежутке от M до N: "+sum);
